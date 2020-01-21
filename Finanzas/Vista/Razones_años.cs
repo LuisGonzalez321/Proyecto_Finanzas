b57d30631a011ad4ff827bcca58a9dd37692024d
Gabx01 <lgabrielgonzaleslopez.lggl@gmail.com>
2020-01-21 16:57:03 -0600

    añadiendo razones entre años

--- Finanzas/Vista/Razones_años.cs
+++ Finanzas/Vista/Razones_años.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Finanzas.Controlador;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -15,6 +16,44 @@ namespace Finanzas.Vista
         public Razones_años ()
         {
             InitializeComponent();
+            lista_años.DisplayMember = "fecha";
+            // lista_años.da = CConsulta.Consultas_query();
+            foreach (string i in CConsulta.Consultas_query())
+            {
+                lista_años.Items.Add(i);
+            }
+        }
+
+        private void btn_salir_Click (object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+        private void btn_minimizar_Click (object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void btn_derecha_Click (object sender, EventArgs e)
+        {
+            if (lista_años.SelectedIndex > -1 && lista_añosSelect.Items.Count < 2)
+            {
+                string año = lista_años.Text;
+                lista_añosSelect.Items.Add(año);
+                int index = lista_años.SelectedIndex;
+                lista_años.Items.Remove(lista_años.SelectedItem);
+            }
+        }
+
+        private void btn_izquierda_Click (object sender, EventArgs e)
+        {
+            if (lista_añosSelect.SelectedIndex > -1)
+            {
+                string año = lista_añosSelect.Text;
+                lista_años.Items.Add(año);
+                int index = lista_añosSelect.SelectedIndex;
+                lista_añosSelect.Items.Remove(lista_añosSelect.SelectedItem);
+            }
         }
     }
 }
